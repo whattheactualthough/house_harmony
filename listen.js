f2da80c35f0e9d0a7b4db131c43de7e69c1e13c2
Lucy Rhodes <lucylovesluke@Lucys-MacBook-Pro.local>
2025-06-02 10:07:14 +0100

    set up file structure

---
+++ listen.js
@@ -0,0 +1,12 @@
+
+const app = require("./app");
+
+const {PORT = 9090} = process.env
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error("Error starting server:", err);
+    return;
+  }
+  console.log(`Server is running on port ${PORT}`);
+});
