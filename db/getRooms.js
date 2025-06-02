6120c45f9065f1eb81e57a83aaf07e0635cbf183
Lucy Rhodes <lucylovesluke@Lucys-MacBook-Pro.local>
2025-06-02 16:22:14 +0100

    add-getRooms-function

---
+++ db/getRooms.js
@@ -0,0 +1,25 @@
+const {createClient} = require("@supabase/supabase-js");
+const {supabase_url, supabase_key} = require("./connection")
+
+async function getRooms() {
+    try {
+      const supabase = createClient(supabase_url, supabase_key);
+      const { data, error } = await supabase
+        .from('rooms')
+        .select('*');
+  
+      if (error) {
+        throw error;
+      }
+      return data;
+    } catch (error) {
+      console.error('Error fetching users:', error);
+      throw error;
+    }
+  }
+
+  getRooms().then((data)=> {
+    console.log(data)
+  })
+
+  module.exports = {getRooms}
