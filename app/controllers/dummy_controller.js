469f2f7e89cdccd069108584b47d177c4a67db1a
Yas887744 <yasin.rashid@outlook.com>
2025-06-02 15:14:12 +0100

    added code for model and controller

--- app/controllers/dummy_controller.js
+++ app/controllers/dummy_controller.js
@@ -1,5 +1,11 @@
 const endpoints = require("../../endpoints.json");
+const { selectTasks } = require("../models/dummy_model");
 
 exports.getApi = (req, res) => {
   res.status(200).send({ endpoints });
+};
+exports.getTasks = (req, res) => {
+  return selectTasks().then((result) => {
+    res.status(200).send(result);
+  });
 };
