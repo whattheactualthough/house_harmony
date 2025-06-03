75d3f473b7830529862b8e04a848f9c03985346f
kiran4788 <kiranmai.mamidala@gmail.com>
2025-06-03 13:54:31 +0100

    Add Users

--- db/getUsers.js
+++ db/getUsers.js
@@ -20,21 +20,4 @@ async function getUsers() {
 }
 module.exports = {getUsers};
 
-    try {
-      const supabase = createClient(supabase_url, supabase_key);
-      const { data, error } = await supabase
-        .from('users')
-        .select('*');
-  
-      if (error) {
-        throw error;
-      }
-      return data;
-    } catch (error) {
-      console.error('Error fetching users:', error);
-      throw error;
-    }
-  }
-
-  module.exports = {getUsers}
-
+   
