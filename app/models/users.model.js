8c94445c0ea58a18f2a9599c56708f3b4950b66e
kiran4788 <kiranmai.mamidala@gmail.com>
2025-06-04 09:57:18 +0100

    Add db call to add users

--- app/models/users.model.js
+++ app/models/users.model.js
@@ -1,9 +1,10 @@
 const { createClient } = require("@supabase/supabase-js");
 const { supabase_url, supabase_key } = require("../../db/connection");
+const supabase = createClient(supabase_url, supabase_key);
 
-exports.selectUsers = async() =>  {
+exports.selectUsers = async () => {
 
-    const supabase = createClient(supabase_url, supabase_key);
+
     const { data, error } = await supabase.from("users").select("*");
 
     if (error) {
@@ -13,31 +14,26 @@ exports.selectUsers = async() =>  {
     return data;
 }
 
-async function addUsers(users) {
-    try {
-        const supabase = createClient(supabase_url, supabase_key);
-        const { data, error } = await supabase
-            .from("users")
-            .insert(users);
+exports.addUsers = async (users) => {
 
-        if (error) {
-            throw error;
-        }
-        return data;
-    } catch (error) {
+    const { data, error } = await supabase
+        .from("users")
+        .insert(users);
+
+    if (error) {
         console.error("Error adding users:", error);
         throw error;
     }
+    return data;
+
 }
 
 /*const users = [{user_name:"Gustavo",group_name:"House Harmony Rd"},
     {user_name:"Yasin",group_name:"House Harmony Rd"},
-    {user_name:"Louie",group_name:"House Harmony Rd"}]
+    {user_name:"Louie",group_name:"House Harmony Rd"}]*/
 
-  addUsers(users)
-  .then(data => console.log("Users added successfully:", data))  
-  .catch(error => console.error("Error adding users:", error));*/
 
-module.exports = { addUsers };
-  
+
+
+
 
