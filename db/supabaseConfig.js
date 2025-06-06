1bf3db56183c6e4c4dcc23e5f338fc6016d280ec
gustavo peretti <gustavo.peretti2001@gmail.com>
2025-06-06 14:13:15 +0100

    updated supabaseConfig

--- db/supabaseConfig.js
+++ db/supabaseConfig.js
@@ -1,8 +1,10 @@
 const { createClient } = require("@supabase/supabase-js");
 const ENV = process.env.NODE_ENV || "development";
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+
 const supabase_url = process.env.SUPABASE_URL;
-const supabase_key = process.env.SUPABASE_SERVICE_KEY;
+const supabase_key = process.env.SUPABASE_KEY;
+
 const supabase = createClient(supabase_url, supabase_key);
 console.log(`Supabase client created for environment: ${ENV}`);
 
